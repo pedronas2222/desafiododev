0daad16064bf6e37f964dcb660adb7576ec09fdc
Pedro Nascimento <125163266+pedronas2222@users.noreply.github.com>
2023-07-05 15:18:07 -0300

    Add files via upload

---
+++ PessoasModel.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Security.Claims;
+
+namespace ApiGlobaltecDev.Models
+{
+    public class PessoasModel
+    {
+
+        [Key]
+        public int Codigo { get; set; }
+
+        [Required]
+        [Column(TypeName = "nvarchar(250)")]
+        public string Nome { get; set; } = "";
+        [Required]
+        [Column(TypeName = "nvarchar(20)")]
+        public string Usuario { get; set; } = "";
+        [Required]
+        [Column(TypeName = "nvarchar(250)")]
+        public string Senha { get; set; } = "";
+        [Required]
+        [Column(TypeName = "nvarchar(14)")]
+        public string? cpfCnpj { get; set; }
+        public string? uf { get; set; }
+        public DateTime DataNascimento { get; set; }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,100 @@
+﻿using ApiGlobaltecDev;
+using ApiGlobaltecDev.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+builder.Services.AddSwaggerGen(); c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "API desafio do DEV", Version = "v1" });
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+    {
+    {
+        new OpenApiSecurityScheme
+        {
+        Reference = new OpenApiReference
+            {
+            Type = ReferenceType.SecurityScheme,
+            Id = "Bearer"
+            },
+            Scheme = "Bearer",
+            Name = "Bearer",
+            In = ParameterLocation.Header,
+
+        },
+        new List<string>()
+        }
+    });
+
+
+});
+
+byte[] key = Encoding.ASCII.GetBytes(ChaveBase.JwtKey);
+
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(x =>
+{
+    x.RequireHttpsMetadata = false;
+    x.SaveToken = true;
+    x.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        ValidateIssuer = false,
+        ValidateAudience = false,
+        ValidateLifetime = false,
+        AuthenticationType = "JWT"
+    };
+});
+
+
+
+
+// AQUI VAMOS CRIAR A DEPENDECIA DE CONEXÃO COM O BANCO DE DADOS.
+builder.Services.AddDbContext<APIDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("ConnectionBase")));
+
+
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.UseAuthentication();
+
+app.MapControllers();
+
+app.Run();
+
